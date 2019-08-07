359ab803c9c5f6bbc13b1d7c4e57d73c73b8c7b6
David Melendez <davidmelendez@knights.ucf.edu>
2019-08-07 10:20:47 -0400

    Test solutionsn to Challenges 1-3

--- test/Set1Spec.hs
+++ test/Set1Spec.hs
@@ -3,11 +3,33 @@ module Set1Spec where
 import Test.Hspec
 import Set1
 import qualified Data.ByteString.Char8 as BC
+import Data.Char
 
 spec :: Spec
-spec = do
-    describe "Set1.hexToBase64" $ do
+spec = describe "Set1" $ do
+    describe "hexToBase64" $ do
         it "converts a hex bytestring to a base64 bytestring" $ do
+            let hexStr = BC.pack "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+            let b64Str = BC.pack "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
             hexToBase64 hexStr `shouldBe` b64Str
-                where hexStr = BC.pack "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-                      b64Str = BC.pack "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+    describe "hexXor" $ do
+        it "XOR's two equal-length hex bytestrings" $ do
+            let str1 = BC.pack "1c0111001f010100061a024b53535009181c"
+            let str2 = BC.pack "686974207468652062756c6c277320657965"
+            let result = BC.pack "746865206b696420646f6e277420706c6179"
+            hexXor str1 str2 `shouldBe` result
+    describe "decryptEnglishSingleByteXor" $ do
+        it "Decrypts an English message encrypted with a single-byte XOR cipher" $ do
+            let str1 = "This is a string of English text."
+            let str2 = "It was a long and stormy night..."
+            let str1' = map toUpper . filter isAlpha $ str1
+            let str2' = map toUpper . filter isAlpha $ str2
+            let bs1 = BC.pack str1
+            let bs2 = BC.pack str2
+            -- Test decryptEnglishSingleByteXor on string XOR's with each possible byte
+            mapM_ (uncurry shouldBe) 
+                [(map toUpper . filter isAlpha . decryptEnglishSingleByteXor . (singleByteXor k) 
+                        $ bs1, str1') | k <- [0..255]]
+            mapM_ (uncurry shouldBe) 
+                [(map toUpper . filter isAlpha . decryptEnglishSingleByteXor . (singleByteXor k) 
+                        $ bs2, str2') | k <- [0..255]]
