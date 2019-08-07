52e4cddc48f8fe8cdc69a40fa94f5ee03910e0aa
David Melendez <davidmelendez@knights.ucf.edu>
2019-08-07 11:05:39 -0400

    Test Challenge 5

--- test/Set1Spec.hs
+++ test/Set1Spec.hs
@@ -28,8 +28,13 @@ spec = describe "Set1" $ do
             let bs2 = BC.pack str2
             -- Test decryptEnglishSingleByteXor on string XOR's with each possible byte
             mapM_ (uncurry shouldBe) 
-                [(map toUpper . filter isAlpha . decryptEnglishSingleByteXor . (singleByteXor k) 
-                        $ bs1, str1') | k <- [0..255]]
+                [(map toUpper . filter isAlpha . decryptEnglishSingleByteXor . (singleByteXor k) $ bs1 
+                        , str1') | k <- [0..255]]
             mapM_ (uncurry shouldBe) 
-                [(map toUpper . filter isAlpha . decryptEnglishSingleByteXor . (singleByteXor k) 
-                        $ bs2, str2') | k <- [0..255]]
+                [(map toUpper . filter isAlpha . decryptEnglishSingleByteXor . (singleByteXor k) $ bs2
+                        , str2') | k <- [0..255]]
+    describe "keyRepeatingXor" $ do
+        it "Encrypts a bytestring using key-repeating XOR" $ do
+            let plain = BC.pack "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+            let cipher = fromHexWith id $ BC.pack "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+            keyRepeatingXor (BC.pack "ICE") plain `shouldBe` cipher
